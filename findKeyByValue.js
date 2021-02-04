edc46309b31650159076255d30c2799b8932025f
Samira Bani Hassan <samira.banihassan@gmail.com>
2021-02-04 01:25:57 +0000

    finds akey in an object by given value

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion is passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑  Assertion is failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+let findKeyByValue = function(obj, value) {
+  for (let item in obj) {
+    if (obj[item] === value) {
+      return item;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
